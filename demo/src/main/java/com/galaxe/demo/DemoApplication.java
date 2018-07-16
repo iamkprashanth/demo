817581fee7590bae4c58fdc7c4666b988435c190
pkanchinakote <pkanchinakote@GBLNB-MP13K5MD.galaxy.lan>
2018-07-16 18:27:38 +0530

    Third commit

--- demo/src/main/java/com/galaxe/demo/DemoApplication.java
+++ demo/src/main/java/com/galaxe/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// This is added by Prashanth K
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
